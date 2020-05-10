c6585aa8d625220d55001ae0e43826fd5bb19335
FaelDi <faeldii@icloud.com>
2020-05-10 02:53:27 -0300

    organizando os diretorios

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/cadastro', function(req, res, next) {
-  res.render('cadastro', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send({ title: 'Servidor Rodando' });
 });
 
 module.exports = router;
