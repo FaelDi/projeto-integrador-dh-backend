4184da84d9693229f4dd85e8676715aa45ad77a5
FaelDi <faeldii@icloud.com>
2020-05-11 19:18:31 -0300

    Alteracao Models

--- back-end/models/Usuario.js
+++ back-end/models/Usuario.js
@@ -1,65 +1,22 @@
 module.exports = (sequelize, DataType) => {
   const Usuario = sequelize.define("Usuario", {
-    // id: {
-    //     type:DataType.INTEGER,
-    //     primaryKey:true,
-    //     autoIncrement:true
-    // },
-    nome: DataType.STRING,
-    email: DataType.STRING,
-    senha: DataType.STRING,
-    cpf: DataType.STRING,
-    idade: {
-      type: DataType.INTEGER,
-      allowNull: true
-    },
-    data_nasc: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    rg: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    // cnpj: {
-    //   type: DataType.STRING,
-    //   allowNull: true
-    // },
-    // fornecedor: {
-    //   type: DataType.STRING,
-    //   allowNull: true
-    // },
-    cep: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    rua: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    numero: {
-      type: DataType.INTEGER,
-      allowNull: true
-    },
-    bairro: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    cidade: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    uf: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    telefone: {
-      type: DataType.STRING,
-      allowNull: true
-    },
-    celular: DataType.STRING
+    nome:      { type: Sequelize.STRING, allowNull: false },//*
+    email:     { type: Sequelize.STRING, allowNull: false},//*
+    senha:     { type: Sequelize.STRING, allowNull: false},//*
+    cpf:       { type: Sequelize.STRING, allowNull: false},//*
+    idade:     { type: Sequelize.STRING, allowNull: true},
+    data_nasc: { type: Sequelize.STRING, allowNull: true},      
+    rg:        { type: Sequelize.STRING, allowNull: true},
+    cep:       { type: Sequelize.STRING, allowNull: true},
+    rua:       { type: Sequelize.STRING, allowNull: true},
+    numero:    { type: Sequelize.INTEGER, allowNull: true},
+    bairro:    { type: Sequelize.STRING, allowNull: true},
+    cidade:    { type: Sequelize.STRING, allowNull: true},
+    uf:        { type: Sequelize.STRING, allowNull: true},
+    telefone:  { type: Sequelize.STRING, allowNull: true},
+    celular:   { type: Sequelize.STRING, allowNull: true}
   }, {
-    tableName: 'usuarios',
+    tableName: 'usuario',
     timestamps: true
   })
 
