70354522b2bd4fa48d6c1536fbbf6cfc881e9ed6
Pedro Freitas <pedrinfreitas@gmail.com>
2020-05-17 18:32:34 -0300

    Criação e ajuste de index, search, new, update e delete - OK.

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -2,31 +2,34 @@ const { Empresa, Usuario } = require("../models");
 const fetch = require("node-fetch");
 
 const buscaCnpj = async(cnpj) => {
-		let res = await fetch(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`);
-		const data =  await res.json();	
-		return data;
+	let res = await fetch(`https://www.receitaws.com.br/v1/cnpj/${cnpj}`);
+	const data =  await res.json();	
+	return data;
 }
 
 module.exports = {
-
-	index: async (req, res)=>{
-		let companies = await Empresa.findAll();
-		if(companies !==  null){
-			res.send(companies);
-		}else{
+	index: async (req, res) => {
+		let empresas = await Empresa.findAll();
+		if (empresas !== null) {
+			res.send(empresas);
+		} else {
 			res.send("Nao há empresas cadastradas");
 		}
 	},
-	// store: (req, res) => {
-  //       let busca = req.params.cnpj;
-  //       let url = 'https://www.receitaws.com.br/v1/cnpj/';
-  //       let urlFetch = url+busca;
-	// 	fetch(urlFetch)
-  //   .then(res => res.json())
-  //   .then(json => res.send(json));
-	// },
-	store: async(req, res) => {	
-    const { id_usuario } = req.params;
+
+	search: async (req, res) => {
+		let id = req.params.id
+
+		let empresa = await Empresa.findByPk(id);
+		if (empresa !== null) {
+			return res.status(200).json(empresa);
+		} else {
+			return res.status(400).json({ err:'Empresa  não foi encontrada!' });
+		}
+	},
+
+	new: async (req, res) => {
+		const { fk_usuario } = req.params;
 		const { cnpj } = req.body;  
 		
 		const { 
@@ -38,21 +41,49 @@ module.exports = {
 		dados.atividade_principal	= atividade_principal[0].text;
 		dados.atividades_secundarias	= atividades_secundarias[0].text;
 		dados.qsa	= qsa[0].nome;
-		dados.fk_usuario	= id_usuario;
+		dados.fk_usuario	= fk_usuario;
 
-		console.log();
-		
-
-		const user = await Usuario.findByPk(id_usuario);
+		const user = await Usuario.findByPk(fk_usuario);
 		
     if(!user) {
       return res.status(400).json({ error: 'Usuario não encontrado!'});
 		}
 		
-
     const empresa = await Empresa.create(dados);
 
-    return res.json(empresa);
-  },
+    return res.status(200).json(empresa);
+	},
+
+	update: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const { ...data } = req.body;
+			const empresa = await Empresa.findByPk(id);
+			
+			if(!empresa) {
+				return res.status(400).json({ error: 'Empresa não encontrado!'});
+			}
+
+			empresa.update(data);
+			return res.status(200).json(empresa);
+
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
+
+	delete: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const empresa = await Empresa.findByPk(id);
+			empresa.destroy();
+
+			return res.status(200).json(empresa);
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
 }
+
+
 
