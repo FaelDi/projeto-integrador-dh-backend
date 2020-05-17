d9f263eb4241dc25612c7a79dd6a89ce2107a40b
Pedro Freitas <pedrinfreitas@gmail.com>
2020-05-17 03:44:19 -0300

    Refatoração no CRUD - testes OK

--- controllers/PagamentoController.js
+++ controllers/PagamentoController.js
@@ -1,52 +1,79 @@
 const { Pagamento, Usuario } = require("../models");
 
 module.exports = {
-	index: async (req, res)=>{
+	index: async (req, res) => {
 		let pagamentos = await Pagamento.findAll({
 			include: [
 				// { model: Usuario, as: 'usuario' },
 			],
 		});
-		if(pagamentos !==  null){
+		if (pagamentos !== null) {
 			res.send(pagamentos);
-		}else{
+		} else {
 			res.send("Nao há pagamentos cadastrados");
 		}
 	},
+
 	search: async (req, res) => {
 		let id = req.params.id
-		
+
 		let pagamento = await Pagamento.findByPk(id);
-		if(pagamento !==  null){
-			res.send(pagamento);
-		}else{
-			res.send("pagamento nao encontrado");
+		if (pagamento !== null) {
+			return res.status(200).json(pagamento);
+		} else {
+			return res.status(400).json({ err:'Usuário  não foi encontrado!' });
 		}
 	},
-//     new: async (req, res) => {		
-// 		const { cpf, ...data } = req.body;		
-					
-// 		let [result] = await Pagamento.findOrCreate({
-// 			where: { cpf: cpf },
-// 			defaults: { ...data }
-// 		})
-			
-// 		if(result !==  null){
-// 			return res.status(200).json(result);  
-// 		}else{
-// 			return res.status(400).json({ err });
-// 		}
-// },
-	delete: async (req, res) => {
-		
-		let id = req.params.id;
-	
-		const deletedPagamento = Pagamento.destroy({
-			where: {
-				id: id
+
+	new: async (req, res) => {
+		try {
+			const { fk_usuario } = req.params;
+			const { numero_cartao, ...data } = req.body;
+
+			const user = await Usuario.findByPk(fk_usuario);
+			if(!user) {
+				return res.status(400).json({ error: 'Usuario não encontrado!'});
 			}
-		})
-		res.redirect("/");
-	}
+
+			const [result] = await Pagamento.findOrCreate({
+				where: { numero_cartao: numero_cartao },
+				defaults: { ...data,  fk_usuario }
+			})
+			return res.status(200).json(result);
+
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
+
+	update: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const { ...data } = req.body;
+			const pagamento = await Pagamento.findByPk(id);
+			
+			if(!pagamento) {
+				return res.status(400).json({ error: 'Pagamento não encontrado!'});
+			}
+
+			pagamento.update(data);
+			return res.status(200).json(pagamento);
+
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
+
+	delete: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const pagamento = await Pagamento.findByPk(id);
+			pagamento.destroy();
+
+			return res.status(200).json(pagamento);
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
 }
 
--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,54 +1,69 @@
 const { Usuario, Pagamento, Empresa } = require("../models");
 
 module.exports = {
-	index: async (req, res)=>{
+	index: async (req, res) => {
 		let users = await Usuario.findAll({
 			include: [
 				{ model: Pagamento, as: 'pagamento' },
+				{ model: Empresa, as: 'empresa' }
 			],
 		});
-		if(users !==  null){
-			res.send(users);
-		}else{
-			res.send("Nao há usuários cadastrados");
+		if (users !== null) {
+			return res.status(200).json(users);
+		} else {
+			return res.status(400).json({ err:'Não existe usuário cadastado!' });
 		}
 	},
+
 	search: async (req, res) => {
 		let id = req.params.id
-		
+
 		let user = await Usuario.findByPk(id);
-		if(user !==  null){
-			res.send(user);
-		}else{
-			res.send("user nao encontrado");
+		if (user !== null) {
+			return res.status(200).json(user);
+		} else {
+			return res.status(400).json({ err:'Usuário  não foi encontrado!' });
 		}
 	},
-    new: async (req, res) => {		
-		const { cpf, ...data } = req.body;		
-					
-		let [result] = await Usuario.findOrCreate({
-			where: { cpf: cpf },
-			defaults: { ...data }
-		})
-			
-		if(result !==  null){
-			return res.status(200).json(result);  
-		}else{
+
+	new: async (req, res) => {
+		try {
+			const { cpf, ...data } = req.body;
+			const [result] = await Usuario.findOrCreate({
+				where: { cpf: cpf },
+				defaults: { ...data }
+			})
+			return res.status(200).json(result);
+
+		} catch{
 			return res.status(400).json({ err });
 		}
-},
+	},
+
+	update: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const { ...data } = req.body;
+			const user = await Usuario.findByPk(id);
+			user.update(data);
+			return res.status(200).json(user);
+
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
+
 	delete: async (req, res) => {
-		
-		let id = req.params.id;
-	
-		const deletedUser = Usuario.destroy({
-			where: {
-				id: id
-			}
-		})
-		res.redirect("/");
-	},companies: (req, res) => {
-		let id = req.params.id;
-	}
+		try {
+			const { id } = req.params;
+			const user = await Usuario.findByPk(id);
+			user.destroy();
+
+			return res.status(200).json(user);
+		} catch{
+			return res.status(400).json({ err });
+		}
+	},
+
 }
 
