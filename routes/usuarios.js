59c83fc7c46a4ec907bd8c03b1cc81b3b3ced498
Pedro Freitas <pedrinfreitas@gmail.com>
2020-05-17 02:53:13 -0300

    Refatoração da rota usuarios - index, search, new, update e delete.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,11 +4,11 @@ var router = express.Router();
 const UsuarioController = require("../controllers/UsuarioController");
 
 router.get('/', UsuarioController.index);
+router.get('/:id',UsuarioController.search);
 router.post("/cadastrar", UsuarioController.new);
-router.delete('/delete/:id',UsuarioController.delete);
-router.get("/:id/companies",UsuarioController.companies);
+router.put('/:id',UsuarioController.update);
+router.delete('/:id',UsuarioController.delete);
+// router.get("/:id/companies",UsuarioController.companies);
 //router.get('/:id/profile',UsuarioController.profile);
-//router.put('/:id/update',UsuarioController.update);
-router.get('/busca/:id',UsuarioController.search);
 
 module.exports = router;
