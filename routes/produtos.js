9aa915526c7642dea720e68aaa3f456df65d4276
Nayara de Souza <nayara.souza@qpro.com.br>
2020-05-18 11:34:27 -0300

    Refatorando as rotas - index, search, new, update e delete.

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,12 +1,12 @@
-
 var express = require('express');
 var router = express.Router();
 
 const ProdutoController = require("../controllers/ProdutoController");
 
 router.get('/', ProdutoController.index);
-//router.post("/cadastrar", ProdutoController.new);
-router.delete('/delete/:id',ProdutoController.delete);
-router.get('/busca/:id',ProdutoController.search);
+router.get('/:id',ProdutoController.search);
+router.post("/cadastrar/:fk_usuario/", ProdutoController.new);
+router.put('/:id',ProdutoController.update);
+router.delete('/:id',ProdutoController.delete);
 
 module.exports = router;
