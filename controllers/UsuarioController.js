8833df974bf076a92496cb95ed3f4a8fc65511e8
Adler Orteney <adler.orteney@fastcash.com.br>
2020-05-23 13:30:35 -0300

    validacao cpf okay

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -2,6 +2,40 @@ const { Usuario, Pagamento, Empresa } = require("../models");
 const jwt = require('../config/jwt');
 const bcrypt = require("bcrypt");
 
+// Função para validar CPF (fonte Receita Federal)
+function validarCPF(cpf) {
+	cpf = cpf.replace(/[^\d]+/g, ''); // Remove qualquer carater nao numerico
+	console.log(cpf)
+	if (cpf == '') return false; // Verifica se não esta vazio
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 ||
+		cpf == "00000000000" ||
+		cpf == "11111111111" ||
+		cpf == "22222222222" ||
+		cpf == "33333333333" ||
+		cpf == "44444444444" ||
+		cpf == "55555555555" ||
+		cpf == "66666666666" ||
+		cpf == "77777777777" ||
+		cpf == "88888888888" ||
+		cpf == "99999999999") {
+		return false;
+	}
+	// Valida 1o digito	
+	add = 0;
+	for (i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+	rev = 11 - (add % 11);
+	if (rev == 10 || rev == 11) rev = 0;
+	if (rev != parseInt(cpf.charAt(9))) return false;
+	// Valida 2o digito	
+	add = 0;
+	for (i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+	rev = 11 - (add % 11);
+	if (rev == 10 || rev == 11) rev = 0;
+	if (rev != parseInt(cpf.charAt(10))) return false;
+	return true;
+};
+
 module.exports = {
 	login: async (req, res) => {
 		const [, hash] = req.headers.authorization.split(' ')
@@ -61,11 +95,13 @@ module.exports = {
 			// Email validation
 			let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
 			if (!data.email.match(regexEmail)) {
-				return res.status(400).json({ result: "Error creating user", message: "The email provided seems not to be valid. Verify it and try again!" });
-			}
+				return res.status(400).json({ result: "Erro ao criar usuário", message: "O email fornecido parece inválido. Verifique e tente novamente!" });
+			};
 
 			// CPF Validation
-
+			if (!validarCPF(cpf)) {
+				return res.status(400).json({ result: "Erro ao criar usuário", message: "O CPF fornecido parece inválido. Verifique e tente novamente!" });
+			};
 
 			const [result] = await Usuario.findOrCreate({
 				where: { cpf: cpf },
@@ -79,7 +115,7 @@ module.exports = {
 
 		} catch (err) {
 			return res.status(200).json({
-				result: "Error creating the user",
+				result: "Erro ao criar usuário",
 				message: err.message
 			});
 		};
