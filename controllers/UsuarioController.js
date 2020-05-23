ec9617f341c129cb2997a9605886ff12b7d9251c
adlermo <adlerorteney@gmail.com>
2020-05-23 12:56:52 -0300

    Validação email, bcrypt senha, e response

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,5 +1,6 @@
 const { Usuario, Pagamento, Empresa } = require("../models");
 const jwt = require('../config/jwt');
+const bcrypt = require("bcrypt");
 
 module.exports = {
 	login: async (req, res) => {
@@ -7,18 +8,18 @@ module.exports = {
 		const [email, senha] = Buffer.from(hash, 'base64')
 			.toString()
 			.split(':')
-		
+
 		try {
-			const user = await Usuario.findOne({ 	
+			const user = await Usuario.findOne({
 				where: { email, senha }
 			})
-	
+
 			if (!user) {
 				return res.send(401)
 			}
-	
+
 			const token = jwt.sign({ user: user.id })
-	
+
 			res.send({ user, token })
 		} catch (error) {
 			res.send(error)
@@ -35,7 +36,7 @@ module.exports = {
 		if (users !== null) {
 			return res.status(200).json(users);
 		} else {
-			return res.status(400).json({ err:'Não existe usuário cadastado!' });
+			return res.status(400).json({ err: 'Não existe usuário cadastado!' });
 		}
 	},
 
@@ -46,25 +47,42 @@ module.exports = {
 		if (user !== null) {
 			return res.status(200).json(user);
 		} else {
-			return res.status(400).json({ err:'Usuário  não foi encontrado!' });
+			return res.status(400).json({ err: 'Usuário  não foi encontrado!' });
 		}
 	},
 
 	new: async (req, res) => {
 		try {
 			const { cpf, ...data } = req.body;
+
+			// Hashes password to store in database
+			data.senha = bcrypt.hashSync(data.senha, 10);
+
+			// Email validation
+			let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+			if (!data.email.match(regexEmail)) {
+				return res.status(400).json({ result: "Error creating user", message: "The email provided seems not to be valid. Verify it and try again!" });
+			}
+
+			// CPF Validation
+
+
 			const [result] = await Usuario.findOrCreate({
 				where: { cpf: cpf },
 				defaults: { ...data }
-			})
+			});
 
-			const token = jwt.sign({ user: user.id })
-		
-			return res.status(200).json(result, token);
+			const token = jwt.sign({ user: result.dataValues.id });
 
-		} catch{
-			return res.status(400).json({ err });
-		}
+			// res.send(cpf, data);
+			return res.status(201).json({ result: result.dataValues, token: token });
+
+		} catch (err) {
+			return res.status(200).json({
+				result: "Error creating the user",
+				message: err.message
+			});
+		};
 	},
 
 	update: async (req, res) => {
