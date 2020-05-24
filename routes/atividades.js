3ac82c6db9176cb22754a040f01e7d0fb2d0e76a
FaelDi <faeldii@icloud.com>
2020-05-24 18:15:15 -0300

    remoção atividades antigas

--- routes/atividades.js
+++ routes/atividades.js
@@ -5,7 +5,7 @@ const AtividadeController = require("../controllers/AtividadeController");
 
 
 router.get('/', AtividadeController.index);
-// router.get('/:id',AtividadeController.search);
+router.get('/:id',AtividadeController.search);
 // router.post("/cadastrar/:fk_empresa/", AtividadeController.new);
 // router.put('/:id',AtividadeController.update);
 // router.delete('/:id',AtividadeController.delete);
