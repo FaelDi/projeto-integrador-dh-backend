82b604ee6cca87a7f905e4a87e650227c5c2eccc
Pedro Freitas <pedrinfreitas@gmail.com>
2020-05-26 20:17:33 -0300

    configurando ejs

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ var avaliacoesRouter = require('./routes/avaliacoes');
 var logMiddleware = require('./middlewares/logSite')
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // app.use(logger('dev'));
 
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 const authMiddleware = require("../middlewares/auth");
 
-router.get('/', authMiddleware, (req, res) => {
-  res.send({ message: 'Servidor Rodando' });
+// router.get('/', authMiddleware, (req, res) => {
+//   res.send({ message: 'Servidor Rodando' });
+// });
+
+router.get('/', (req, res) => {
+  res.render('index', { msg: 'teste' });
 });
 
 router.get('/me', authMiddleware, (req, res) => {
