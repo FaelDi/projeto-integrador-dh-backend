7e8b6635b736308d8a25c61c48e8a960a1fe0063
Pedro Freitas <pedrinfreitas@gmail.com>
2020-05-29 03:45:14 -0300

    Ajuste na rota empresas

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { Empresa } = require("../models");
 
 const authMiddleware = require("../middlewares/auth");
 
@@ -10,6 +11,20 @@ const authMiddleware = require("../middlewares/auth");
 router.get('/', (req, res) => {
   res.render('index', { msg: 'teste' });
 });
+
+/* !!!!!!!!! COLOQUEI EMPRESA PARA EVITAR CONFLITO COM EMPRESAS */ 
+router.get('/empresa', async (req, res) => {
+  let empresas = await Empresa.findAll({
+    include: 'atividade'
+  })
+  if (empresas) {
+    // res.send(empresas);
+    res.render('empresas', { data: empresas });
+  } else {
+    res.send("Nao há empresas cadastradas");
+  };
+});  
+
 
 router.get('/logon', (req, res) => {
   res.render('logon', { msg: 'logon' });
