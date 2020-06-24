9c6eafae86198ab72af71d8888e78f86617812c7
rafael.ferreira <rafael.ferreira@geofusion.com.br>
2020-06-24 15:38:15 -0300

    comentando middleware

--- routes/entregas.js
+++ routes/entregas.js
@@ -6,7 +6,7 @@ const EntregaController = require("../controllers/EntregaController");
 
 router.get('/', authMiddleware, EntregaController.index);
 //router.post("/cadastrar", authMiddleware, EntregaController.new);
-router.delete('/delete/:id', authMiddleware, EntregaController.delete);
-router.get('/busca/:id', authMiddleware, EntregaController.search);
+router.delete('/delete/:id',  /*authMiddleware,*/ EntregaController.delete);
+router.get('/busca/:id',  /*authMiddleware,*/EntregaController.search);
 
 module.exports = router;
