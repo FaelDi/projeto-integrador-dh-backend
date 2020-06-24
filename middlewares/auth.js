0f68519b5ea4a081e6d2879103088d2c9e8e0ac8
adlermo <adlerorteney@gmail.com>
2020-06-24 00:25:18 -0300

    Modulos e mensagens de login tratados

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,9 @@
 const { Usuario, Pagamento, Empresa } = require("../models");
 
-const authMiddleware = async (req, res, next) => {
-  try {
-    // [, token] taken the second element splited
-    const [, token] = req.headers.authorization ? req.headers.authorization.split(' ') : null;
-    // console.log(req.headers.authorization)
-    // console.log(payload.user);
-
-    const user = await Usuario.findByPk(payload.user);
-    // console.log(user);
-
-    if (!user) {
-      return res.send(401);
-    };
-
-    req.auth = user;
-
+module.exports = (req, res, next) => {
+  if (req.session.user) {
     next();
-  } catch (error) {
-    console.log(error)
-    res.sendStatus(401);
+  } else {
+    res.render("login", { err: "É necessário login para acessar a página!", display: '' });
   };
 };
-
-module.exports = authMiddleware;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const { Empresa } = require("../models");
+
+const auth = require('../middlewares/auth');
 
 router.get('/', (req, res) => {
   res.render('index', { msg: 'teste' });
@@ -9,14 +10,14 @@ router.get('/', (req, res) => {
 /* !!!!!!!!! COLOQUEI EMPRESA PARA EVITAR CONFLITO COM EMPRESAS */ // MOVIDO PARA EmpresaController.js
 
 router.get('/login', (req, res) => {
-  res.render('login', { err: '', display: "hidden" });
+  res.render('login', { err: 'Enviar', display: "hidden" });
 });
 
 router.get('/register', (req, res) => {
   res.render('register', { err: '' });
 });
 
-router.get('/me', (req, res) => {
+router.get('/me', auth, (req, res) => {
   res.send(req.auth);
 });
 
